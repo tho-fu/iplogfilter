84196645f24803812688091052dded0aa91e59fc
tho-fu <91804302+tho-fu@users.noreply.github.com>
2021-10-12 17:24:23 +0200

    Added mail function
    Added mail function to send results via smtp server

--- iplogfilter.py
+++ iplogfilter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-version = "0.15"
+version = "0.16"
 
 __author__ = "ThoFu"
 __copyright__ = "Copyright 2021, ThoFu"
@@ -11,15 +11,36 @@ __version__ = version
 __maintainer__ = "ThoFu"
 __website__ = "https://github.com/tho-fu/iplogfilter"
 
+############# Config Variables #############
+iplist = ['192.168.1.0/24','10.10.10.0/24']
+
+mailconfig = "1" # 1 = send over smtp-server
+############# Config Variables #############
+
+if mailconfig == "1":
+    ########## Mail Config Variables ##########
+    csmtpserver = 'smtp.example.com'
+    csmtpport = 'xx'
+    csmtpuser = 'user@example.com'
+    csmtppass = 'examplepass'
+    cmailsubject = 'Logfileanalysis'
+    cmailfrom = 'sender@example.com'
+    ########## Mail Config Variables ##########
+else:
+    print ("No smtp-server given")
+    raise SystemExit()
+
 import re
 import sys
 import ipaddress
 import os
 import argparse
 
-############# Config Variables #############
-iplist = ['192.168.1.0/24','10.10.10.0/24']
-############# Config Variables #############
+if mailconfig == "1":
+    import smtplib
+    from email.utils import formatdate
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
 
 class bcolors:
     OKGREEN = '\033[92m'
@@ -44,6 +65,53 @@ def withopen(file):
     fh.close()
     return fstring
 
+def checkmailaddress(mailaddress):
+    if (re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b',args.mail)):
+        checkmail = True
+    else:
+        print ("Given mail address is not valid!")
+        raise SystemExit()
+    return checkmail
+
+def smtpsendoutmail(mailfor, mailmessage, mailcheckmail, mailsubject, mailfrom):
+    try:
+        if mailcheckmail == True:
+            #Create your SMTP session 
+            smtp = smtplib.SMTP(csmtpserver, csmtpport)
+
+            #Use TLS to add security 
+            smtp.starttls()
+
+            #User Authentication 
+            smtp.login(csmtpuser,csmtppass)
+
+            msg = MIMEMultipart()
+
+            msg['Subject'] = mailsubject
+            msg['From'] = mailfrom
+            msg['To'] = mailfor
+            msg["Date"] = formatdate(localtime=True)
+
+            msg.attach(MIMEText(mailmessage, 'html'))
+    
+            #Sending the Email
+            smtp.sendmail(mailfrom, mailfor,msg.as_string())
+
+            #Terminating the session
+            smtp.quit()
+            print ("Email sent successfully!")
+
+    except Exception as ex:
+        print("Something went wrong....",ex)
+
+def checkmailconfig():
+    if csmtpport != "xx" and csmtppass != "examplepass":
+        check = True
+        return check
+    else:
+        print ("Mail Config not ok, please check!")
+        raise SystemExit()
+
 my_parser = argparse.ArgumentParser(
     description='Search for interesting IP-Ranges within your logfiles',
     add_help=True,
@@ -52,6 +120,7 @@ my_parser = argparse.ArgumentParser(
 
 my_parser.add_argument('-v', '--version', action='version', version='%(prog)s {version}'.format(version=__version__))
 my_parser.add_argument('-t', '--filetype', type=int, metavar='', required=True, help='input filetype - 1 = Fail2Ban - 2 = AuthLog - 3 = OPNSense - 4 = IPTables')
+my_parser.add_argument('-m', '--mail', type=str, metavar='', help='mail the result (only if) to this address')
 
 my_parser.add_argument('logfile', type=str, help='logfile name and path')
 
@@ -90,10 +159,18 @@ if args.filetype and args.logfile:
                 lst.sort(reverse=False)
                 print("\n" + bcolors.OKGREEN + bcolors.UNDERLINE + "Result:" + bcolors.ENDC + "\n")
                 print ("{:<30} {:<26} {:<20}".format('Description','Time','IP'))
+                mailmsg = "<table><tr><th align='left'>Description &nbsp;</th><th align='left'>Time &nbsp;</th><th align='left'>IP &nbsp;</th></tr>"
                 for v in lst:
                     ipdescr, iptime, ipip = v
                     print ("{:<30} {:<26} {:<20}".format( ipdescr, iptime, ipip))
+                    mailmsg = mailmsg + "<tr><td align='left'>" + ipdescr + " &nbsp;</td><td align='left'>" + iptime + " &nbsp;</td><td align='left'>" + ipip + " &nbsp;</td></tr>"
                 print("\n")
+                mailmsg = mailmsg + "</table>"
+
+                if args.mail:
+                    if checkmailconfig() and checkmailaddress(args.mail) is True:
+                        if mailconfig == "1":
+                            smtpsendoutmail(args.mail,mailmsg,checkmailaddress(args.mail),cmailsubject + ' - fail2ban',cmailfrom)
 
         elif args.filetype == 2:
         
@@ -118,10 +195,18 @@ if args.filetype and args.logfile:
                 lst.sort(reverse=False)
                 print("\n" + bcolors.OKGREEN + bcolors.UNDERLINE + "Result:" + bcolors.ENDC + "\n")
                 print ("{:<30} {:<26} {:<20}".format('Description','Time','IP'))
+                mailmsg = "<table><tr><th align='left'>Description &nbsp;</th><th align='left'>Time &nbsp;</th><th align='left'>IP &nbsp;</th></tr>"
                 for v in lst:
                     ipdescr, iptime, ipip = v
                     print ("{:<30} {:<26} {:<20}".format( ipdescr, iptime, ipip))
+                    mailmsg = mailmsg + "<tr><td align='left'>" + ipdescr + " &nbsp;</td><td align='left'>" + iptime + " &nbsp;</td><td align='left'>" + ipip + " &nbsp;</td></tr>"
                 print("\n")
+                mailmsg = mailmsg + "</table>"
+
+                if args.mail:
+                    if checkmailconfig() and checkmailaddress(args.mail) is True:
+                        if mailconfig == "1":
+                            smtpsendoutmail(args.mail,mailmsg,checkmailaddress(args.mail),cmailsubject + ' - auth',cmailfrom)
 
         elif args.filetype == 3:
 
@@ -167,11 +252,19 @@ if args.filetype and args.logfile:
                 print("\n" + bcolors.OKGREEN + bcolors.UNDERLINE + "Result:" + bcolors.ENDC + "\n")
                 print ("{:<15} {:<24} {:<20} {:<20} {:<10} {:<14} {:<14} {:<10} {:<18} {:<10}".format('Description','Time','SourceIP','DestIP','Direction',
                     'SourcePort','DestPort','Action','Interface','Proto'))
+                mailmsg = "<table><tr><th align='left'>Description &nbsp;</th><th align='left'>Time &nbsp;</th><th align='left'>SourceIP &nbsp;</th><th align='left'>DestIP &nbsp;</th><th align='left'>Direction &nbsp;</th><th align='left'>SourcePort &nbsp;</th><th align='left'>DestPort &nbsp;</th><th align='left'>Action &nbsp;</th><th align='left'>Interface &nbsp;</th><th align='left'>Proto &nbsp;</th></tr>"
                 for v in lst:
                     ipdescr, iptime, ipsrcip, ipdstip, ipinorout, ipsrcport, ipdstport, ippassorblock, ipinterface, ipproto = v
                     print ("{:<15} {:<24} {:<20} {:<20} {:<10} {:<14} {:<14} {:<10} {:<18} {:<10}".format( ipdescr, iptime, ipsrcip, ipdstip, ipinorout, 
                         ipsrcport, ipdstport, ippassorblock, ipinterface, ipproto))
+                    mailmsg = mailmsg + "<tr><td align='left'>" + ipdescr + " &nbsp;</td><td align='left'>" + iptime + " &nbsp;</td><td align='left'>" + ipsrcip + " &nbsp;</td><td align='left'>" + ipdstip + " &nbsp;</td><td align='left'>" + ipinorout + " &nbsp;</td><td align='left'>" + ipsrcport + " &nbsp;</td><td align='left'>" + ipdstport + " &nbsp;</td><td align='left'>" + ippassorblock + " &nbsp;</td><td align='left'>" + ipinterface + " &nbsp;</td><td align='left'>" + ipproto + " &nbsp;</td></tr>"
                 print("\n")
+                mailmsg = mailmsg + "</table>"
+
+                if args.mail:
+                    if checkmailconfig() and checkmailaddress(args.mail) is True:
+                        if mailconfig == "1":
+                            smtpsendoutmail(args.mail,mailmsg,checkmailaddress(args.mail),cmailsubject + ' - opnsense',cmailfrom)
 
         elif args.filetype == 4:
 
@@ -216,11 +309,19 @@ if args.filetype and args.logfile:
                 print("\n" + bcolors.OKGREEN + bcolors.UNDERLINE + "Result:" + bcolors.ENDC + "\n")
                 print ("{:<20} {:<20} {:<20} {:<20} {:<10} {:<10} {:<14} {:<14} {:<10}".format('Description','Time','SourceIP','DestIP','IN',
                     'Out','SourcePort','DestPort','Proto'))
+                mailmsg = "<table><tr><th align='left'>Description &nbsp;</th><th align='left'>Time &nbsp;</th><th align='left'>SourceIP &nbsp;</th><th align='left'>DestIP &nbsp;</th><th align='left'>IN &nbsp;</th><th align='left'>Out &nbsp;</th><th align='left'>SourcePort &nbsp;</th><th align='left'>DestPort &nbsp;</th><th align='left'>Proto &nbsp;</th></tr>"
                 for v in lst:
                     ipdescr, iptime, ipsrcip, ipdstip, ipin, ipout, ipsrcport, ipdstport, ipproto = v
                     print ("{:<20} {:<20} {:<20} {:<20} {:<10} {:<10} {:<14} {:<14} {:<10}".format( ipdescr, iptime, ipsrcip, ipdstip, ipin, 
                         ipout, ipsrcport, ipdstport, ipproto))
+                    mailmsg = mailmsg + "<tr><td align='left'>" + ipdescr + " &nbsp;</td><td align='left'>" + iptime + " &nbsp;</td><td align='left'>" + ipsrcip + " &nbsp;</td><td align='left'>" + ipdstip + " &nbsp;</td><td align='left'>" + ipin + " &nbsp;</td><td align='left'>" + ipout + " &nbsp;</td><td align='left'>" + ipsrcport + " &nbsp;</td><td align='left'>" + ipdstport + " &nbsp;</td><td align='left'>" + ipproto + " &nbsp;</td></tr>"
                 print("\n")
+                mailmsg = mailmsg + "</table>"
+
+                if args.mail:
+                    if checkmailconfig() and checkmailaddress(args.mail) is True:
+                        if mailconfig == "1":
+                            smtpsendoutmail(args.mail,mailmsg,checkmailaddress(args.mail),cmailsubject + ' - iptables',cmailfrom)
 
         else:
             print ("Bad Filetype")
